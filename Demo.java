afba100a2b24fb5e9f03578907f96238e2fded8b
James Smith Wigglesworth <jamesmithwigglesworth@gmail.com>
2023-11-03 00:22:18 +0700

    first commit: pushed source code

---
+++ Demo.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+import com.cbis.CBIS;
+
+public class Demo {
+    public static void main(String[] args) {
+        // String[] sizes = {"kecil", "sedang", "besar"};
+        // String[] statuses = {"sorted", "random", "reversed"};
+
+        String size = "sedang";
+        String status = "random";
+        
+        int[] arr = Generator.generateArray(size, status);
+
+        long startTime = System.nanoTime();
+        int[] sortedArr = CBIS.sort(arr);
+        long endTime = System.nanoTime();
+
+        double ms = (endTime - startTime) / 1_000_000.0;
+
+        System.out.printf("Size: %s; Status: %s; Time elapsed: %f ms%n", size, status, ms);
+
+
+        // for (String size: sizes) {
+        //     for (String status: statuses) {
+        //         int[] arr = Generator.generateArray(size, status);
+
+        //         long startTime = System.nanoTime();
+        //         int[] sortedArr = CBIS.sort(arr);
+        //         long endTime = System.nanoTime();
+
+        //         System.out.printf("Size: %s; Status: %s; Time elapsed: %d ns\n", size, status, (endTime-startTime));
+        //     }
+        // }
+        // System.out.println(Arrays.toString(sortedArr));
+    }
+}
+
+class Generator {
+    public static int[] generateArray(String size, String status) {
+        int n = 1;
+        Random rand = new Random();
+
+        if (size.equals("kecil")) { //
+            n = 200;
+        }
+        else if (size.equals("sedang")) {
+            n = 2000;
+        }
+        else if (size.equals("besar")) {
+            n = 20000;
+        }
+
+        Integer[] arrOut = new Integer[n];
+        
+        for (int i = 0; i < n; i++) {
+            arrOut[i] = rand.nextInt(1, Integer.MAX_VALUE);
+        }
+
+        if (status.equals("sorted")) {
+            Arrays.sort(arrOut);
+        }
+        else if (status.equals("reversed")) {
+            Arrays.sort(arrOut, Collections.reverseOrder());
+        } else if (status.equals("random")) {
+            ;
+        }
+
+        return Arrays.stream(arrOut).mapToInt(Integer::intValue).toArray();
+    }
+}
---
+++ com/cbis/CBIS.java
@@ -0,0 +1,61 @@
+package com.cbis;
+
+public class CBIS {
+
+    public static int[] sort(int[] a_list) {
+        int POP = 0;
+
+        for (int i = 1; i <= a_list.length-1; i++) {
+            int COP = i;
+            int key = a_list[COP];
+            int place;
+
+            if (key >= a_list[POP]) { // left or right movement decision
+                place = binary_loc_finder(a_list, POP+1, COP-1, key); // move right
+            }
+            else {
+                place = binary_loc_finder(a_list, 0, POP-1, key); // move left
+            }
+            POP = place;
+            // System.out.printf("POP val: %d || COP val: %d\n", POP, COP);
+            a_list = place_inserter(a_list, POP, COP);
+        }
+        return a_list;
+    }
+    public static int binary_loc_finder(int[] a_list, int start, int end, int key) {
+        if (start == end) {
+            if (a_list[start] > key) {
+                return start;
+            } 
+            else {
+                return start + 1;
+            }
+        }
+
+        if (start > end) {
+            return start;
+        }
+        else {
+            int middle = (start + end) / 2;
+            
+            if (a_list[middle] < key) {
+                return binary_loc_finder(a_list, middle+1, end, key);
+            }
+            else if (a_list[middle] > key) {
+                return binary_loc_finder(a_list, start, middle-1, key);
+            }
+            else {
+                return middle;
+            }
+        }
+    }
+    public static int[] place_inserter(int[] a_list, int start, int end) {
+        int temp = a_list[end];
+
+        for (int k = end; k > start; k--) {
+            a_list[k] = a_list[k-1];
+        }
+        a_list[start] = temp;
+        return a_list;
+    }
+}
